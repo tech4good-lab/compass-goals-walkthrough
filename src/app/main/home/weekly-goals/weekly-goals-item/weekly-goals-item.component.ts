79f5d6f5fc5ffcf0bf13baa6d7f12929a4dbdee8
audsostrom <audreyostrom64@gmail.com>
2024-07-12 13:34:08 -0700

    feat(general): adding inputs to weekly goals item

--- src/app/main/home/weekly-goals/weekly-goals-item/weekly-goals-item.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals-item/weekly-goals-item.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { WeeklyGoalsItemAnimations } from './weekly-goals-item.animations';
+import { ChangeDetectionStrategy, Component, OnInit, input, output } from '@angular/core';
+import { Timestamp } from '@angular/fire/firestore';
 import { MatCheckbox } from '@angular/material/checkbox';
+import { WeeklyGoalData } from '../../home.model';
+import { WeeklyGoalsItemAnimations } from './weekly-goals-item.animations';
 
 @Component({
   selector: 'app-weekly-goals-item',
@@ -15,6 +17,32 @@ import { MatCheckbox } from '@angular/material/checkbox';
 })
 export class WeeklyGoalsItemComponent implements OnInit {
   // --------------- INPUTS AND OUTPUTS ------------------
+
+  // some hard-coded data for now...
+  sampleData: WeeklyGoalData = {
+    __id: 'wg1',
+    __userId: 'test-user',
+    __quarterlyGoalId: 'qg1',
+    __hashtagId: 'ht1',
+    text: 'Finish Google cover letter',
+    completed: false,
+    order: 1,
+    _createdAt: Timestamp.now(),
+    _updatedAt: Timestamp.now(),
+    _deleted: false,
+    hashtag: {
+      __id: 'ht1',
+      name: 'coverletter',
+      color: '#EE8B72',
+      _createdAt: Timestamp.now(),
+      _updatedAt: Timestamp.now(),
+      _deleted: false,
+    },
+  };
+
+  // ... which we'll then pass as the input to this component!
+  goal = input<WeeklyGoalData>(this.sampleData);
+  checked = output<WeeklyGoalData>();
 
   // --------------- LOCAL UI STATE ----------------------
 
