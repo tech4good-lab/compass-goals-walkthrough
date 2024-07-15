9057982acdbab26c05eb5663cb903c31dd026d6f
audsostrom <audreyostrom64@gmail.com>
2024-07-15 15:20:14 -0700

    fix(general): missing import

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,5 +1,6 @@
 import { inject, effect, Component, OnInit, ChangeDetectionStrategy, Signal } from '@angular/core';
 import { HomeAnimations } from './home.animations';
+import { DateTimeComponent } from './date-time/date-time.component';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { HomeAnimations } from './home.animations';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   animations: HomeAnimations,
+  imports: [DateTimeComponent]
 })
 export class HomeComponent implements OnInit {
   // --------------- INPUTS AND OUTPUTS ------------------
