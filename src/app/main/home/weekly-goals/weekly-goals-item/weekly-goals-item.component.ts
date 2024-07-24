9a025c5467380f7019663c711ad0d3144b349b35
audsostrom <audreyostrom64@gmail.com>
2024-07-24 11:03:17 -0700

    feat(general): added on click

--- src/app/main/home/weekly-goals/weekly-goals-item/weekly-goals-item.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals-item/weekly-goals-item.component.ts
@@ -3,6 +3,7 @@ import { WeeklyGoalsItemAnimations } from './weekly-goals-item.animations';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { WeeklyGoalData } from '../../home.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-weekly-goals-item',
@@ -27,9 +28,22 @@ export class WeeklyGoalsItemComponent implements OnInit {
 
   // --------------- EVENT HANDLING ----------------------
 
+  checkGoal() {
+    this.snackBar.open(
+      `Clicked on goal "${this.goal().text}"`,
+      '',
+      {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      },
+    );
+  }
+
   // --------------- OTHER -------------------------------
 
   constructor(
+    private snackBar: MatSnackBar,
   ) { }
 
   // --------------- LOAD AND CLEANUP --------------------
