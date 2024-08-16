6f5826ed2f2e68fbbb759c9f55c8fc0c8979f4ef
audsostrom <audreyostrom64@gmail.com>
2024-08-16 14:48:27 -0700

    feat(general): updating backend entities

--- src/app/main/home/weekly-goals/weekly-goals.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, Signal, computed, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit, Signal, computed, inject } from '@angular/core';
 import { WeeklyGoalsAnimations } from './weekly-goals.animations';
 import { WeeklyGoalsHeaderComponent } from './weekly-goals-header/weekly-goals-header.component';
 import { WeeklyGoalsItemComponent } from './weekly-goals-item/weekly-goals-item.component';
@@ -17,6 +17,7 @@ import { LoadQuarterlyGoal, QuarterlyGoalStore } from 'src/app/core/store/quarte
 import { getStartWeekDate } from 'src/app/core/utils/time.utils';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatButton } from '@angular/material/button';
+import { BATCH_WRITE_SERVICE, BatchWriteService } from 'src/app/core/store/batch-write.service';
 
 @Component({
   selector: 'app-weekly-goals',
@@ -126,6 +127,20 @@ export class WeeklyGoalsComponent implements OnInit {
         incompleteGoals: this.incompleteWeeklyGoals(),
         updateWeeklyGoals: async (weeklyGoalsFormArray) => {
           try {
+            await Promise.all(weeklyGoalsFormArray.controls.map(async (control, i) => {
+              // if this is a new quarter goal
+              if (!control.value.__weeklyGoalId) {
+                await this.addNewGoal(control.value, i);
+              // if it's a goal that's getting deleted
+              } else if (control.value._deleted) {
+                await this.removeGoal(control.value);
+              // if it's a goal that's getting updated
+              } else {
+                await this.updateGoal(control.value, i);
+              }
+            }));
+  
+            this.dialogRef.close();
             this.snackBar.open(
               `Goals were updated`,
               '',
@@ -146,9 +161,42 @@ export class WeeklyGoalsComponent implements OnInit {
 
   // --------------- OTHER -------------------------------
 
+  /** Helper function for adding a new goal to the batch write */
+  async addNewGoal(controlValue, i) {
+    // Add a quarterly goal
+    await this.weeklyGoalStore.add(Object.assign({}, {
+      __userId: this.currentUser().__id,
+      __quarterlyGoalId: controlValue.__quarterlyGoalId,
+      text: controlValue.text,
+      completed: false,
+      order: i + 1,
+      _deleted: controlValue._deleted,
+    }));
+  }
+
+  /** Helper function for removing a goal in the batch write */
+  async removeGoal(controlValue) {
+    // no restrictions on deleting weekly goals, unlike quarterly goals
+    await this.weeklyGoalStore.remove(controlValue.__weeklyGoalId);
+  }
+
+  /** Helper function for updating a goal in the batch write */
+  async updateGoal(controlValue, i) {
+    // text or quarterly goal has changed, general update
+    if (controlValue.originalText !== controlValue.text || controlValue.originalOrder !== i+1 || (!controlValue.originalQuarterlyGoalId && controlValue.__quarterlyGoalId)) {
+      await this.weeklyGoalStore.update(controlValue.__weeklyGoalId, Object.assign({}, {
+        __quarterlyGoalId: controlValue.__quarterlyGoalId,
+        text: controlValue.text,
+        order: i + 1,
+        _deleted: controlValue._deleted,
+      }));
+    }
+  }
+
   constructor(
     private snackBar: MatSnackBar,
     private dialog: MatDialog,
+
   ) { }
 
   // --------------- LOAD AND CLEANUP --------------------
