6792831e8ef9578c8e3614f266125357a8793df6
Audrey Ostrom <audreyostrom64@gmail.com>
2024-12-13 11:50:35 -0800

    static view for card and global styles

--- src/app/main/home/home.model.ts
+++ src/app/main/home/home.model.ts
@@ -1,6 +1,13 @@
 import { WeeklyGoal } from "src/app/core/store/weekly-goal/weekly-goal.model";
 import { Hashtag } from "src/app/core/store/hashtag/hashtag.model";
+import { QuarterlyGoal } from "src/app/core/store/quarterly-goal/quarterly-goal.model";
 
 export interface WeeklyGoalData extends WeeklyGoal {
   hashtag: Hashtag;
 }
+
+export interface QuarterlyGoalData extends QuarterlyGoal {
+   hashtag: Hashtag;
+   weeklyGoalsTotal: number;
+   weeklyGoalsComplete: number;
+ }
--- src/app/main/home/weekly-goals/weekly-goals.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { WeeklyGoalsAnimations } from './weekly-goals.animations';
 import { WeeklyGoalsItemComponent } from './weekly-goals-item/weekly-goals-item.component';
-import { WeeklyGoalData } from '../home.model';
+import { QuarterlyGoalData, WeeklyGoalData } from '../home.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { WeeklyGoalsHeaderComponent } from "./weekly-goals-header/weekly-goals-header.component";
 
@@ -12,27 +12,113 @@ import { WeeklyGoalsHeaderComponent } from "./weekly-goals-header/weekly-goals-h
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: WeeklyGoalsAnimations,
   standalone: true,
-  imports: [WeeklyGoalsItemComponent, WeeklyGoalsHeaderComponent],
+  imports: [
+    /** Components */
+    WeeklyGoalsHeaderComponent,
+    WeeklyGoalsItemComponent,
+  ],
 })
 export class WeeklyGoalsComponent implements OnInit {
   // --------------- INPUTS AND OUTPUTS ------------------
 
   // --------------- LOCAL UI STATE ----------------------
 
-  sampleData: WeeklyGoalData = {
-    __id: 'wg1',
-    __userId: 'test-user',
-    __quarterlyGoalId: 'qg1',
-    __hashtagId: 'ht1',
-    text: 'Finish Google Cover Letter',
-    completed: false,
-    order: 1,
-    hashtag: {
-      __id: 'ht1',
-      name: 'coverletter',
-      color: '#EE8B72',
+  incompleteWeeklyGoals: WeeklyGoalData[] = [
+    {
+      __id: 'wg1',
+      __userId: 'test-user',
+      __quarterlyGoalId: 'qg1',
+      __hashtagId: 'ht1',
+      text: 'Finish Google Cover Letter',
+      completed: false,
+      order: 1,
+      hashtag: {
+        __id: 'ht1',
+        name: 'coverletter',
+        color: '#EE8B72',
+      },
     },
-  };
+    {
+      __id: 'wg2',
+      __userId: 'test-user',
+      __quarterlyGoalId: 'qg2',
+      __hashtagId: 'ht2',
+      text: 'Apply to Microsoft',
+      completed: false,
+      order: 2,
+      hashtag: {
+        __id: 'ht2',
+        name: 'apply',
+        color: '#2DBDB1',
+      },
+    },
+  ];
+
+  completeWeeklyGoals: WeeklyGoalData[] = [
+    {
+      __id: 'wg3',
+      __userId: 'test-user',
+      __quarterlyGoalId: 'qg3',
+      __hashtagId: 'ht3',
+      text: 'Review data structures',
+      completed: true,
+      order: 3,
+      hashtag: {
+        __id: 'ht3',
+        name: 'interview',
+        color: '#FFB987',
+        _deleted: false,
+      },
+    },
+  ];
+
+  quarterlyGoals: QuarterlyGoalData[] = [
+    {
+      __id: 'qg1',
+      __userId: 'test-user',
+      __hashtagId: 'ht1',
+      text: 'Finish cover letters',
+      completed: false,
+      order: 1,
+      hashtag: {
+        __id: 'ht1',
+        name: 'coverletter',
+        color: '#EE8B72',
+      },
+      weeklyGoalsTotal: 1,
+      weeklyGoalsComplete: 0,
+    },
+    {
+      __id: 'qg2',
+      __userId: 'test-user',
+      __hashtagId: 'ht2',
+      text: 'Apply to internships',
+      completed: false,
+      order: 2,
+      hashtag: {
+        __id: 'ht2',
+        name: 'apply',
+        color: '#2DBDB1',
+      },
+      weeklyGoalsTotal: 1,
+      weeklyGoalsComplete: 0,
+    },
+    {
+      __id: 'qg3',
+      __userId: 'test-user',
+      __hashtagId: 'ht3',
+      text: 'Technical interview prep!',
+      completed: false,
+      order: 3,
+      hashtag: {
+        __id: 'ht3',
+        name: 'interview',
+        color: '#FFB987',
+      },
+      weeklyGoalsTotal: 1,
+      weeklyGoalsComplete: 1,
+    },
+  ];
 
   // --------------- COMPUTED DATA -----------------------
 
@@ -64,7 +150,9 @@ export class WeeklyGoalsComponent implements OnInit {
 
   // --------------- OTHER -------------------------------
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(
+    private snackBar: MatSnackBar,
+  ) {}
 
   // --------------- LOAD AND CLEANUP --------------------
 
