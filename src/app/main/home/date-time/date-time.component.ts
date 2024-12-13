2fc4272e40b003139652557cfc0c6bc8d97f00df
Audrey Ostrom <audreyostrom64@gmail.com>
2024-12-13 14:28:26 -0800

    component finished

--- src/app/main/home/date-time/date-time.component.ts
+++ src/app/main/home/date-time/date-time.component.ts
@@ -1,5 +1,8 @@
-import { Component,OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component,OnInit, ChangeDetectionStrategy, computed, Signal } from '@angular/core';
 import { DateTimeAnimations } from './date-time.animations';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { interval, startWith, map } from 'rxjs';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-date-time',
@@ -8,7 +11,7 @@ import { DateTimeAnimations } from './date-time.animations';
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: DateTimeAnimations,
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
 })
 export class DateTimeComponent implements OnInit {
   // --------------- INPUTS AND OUTPUTS ------------------
@@ -16,6 +19,31 @@ export class DateTimeComponent implements OnInit {
   // --------------- LOCAL UI STATE ----------------------
 
   // --------------- COMPUTED DATA -----------------------
+
+  /** Current time as signal */
+  time: Signal<Date> = toSignal(
+    interval(1000).pipe(
+      startWith(0),
+      map(() => new Date())
+    ),
+    { requireSync: true }
+  );
+
+  /** Date suffix based on the day of month */
+  dateSuffix: Signal<string> = computed(() => {
+    const currentDay = this.time().getDate();
+    if (currentDay > 3 && currentDay < 21) return 'th';
+    switch (currentDay % 10) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  });
 
   // --------------- EVENT HANDLING ----------------------
 
